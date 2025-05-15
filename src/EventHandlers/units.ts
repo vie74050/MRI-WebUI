01e805a5fbdfdbd87ce3af520f522b3fea0929df
vie_bcit <vienna_ly@bcit.ca>
2025-05-15 15:19:16 -0700

    #1 metric to US conversion

--- src/EventHandlers/units.ts
+++ src/EventHandlers/units.ts
@@ -1,21 +1,61 @@
-// Get #units select and assign change event
+// HTML elements
 const unitsSelect = document.getElementById("units") as HTMLSelectElement;
-const unitsFt = document.getElementById("unitsFt") as HTMLDivElement;
-const unitsIn = document.getElementById("unitsIn") as HTMLDivElement;
-const unitsLb = document.getElementById("unitsLb") as HTMLDivElement;
 
-// add event listener to units select element
+const heightInputFt = document.getElementById("heightFt") as HTMLInputElement;
+const heightInputIn = document.getElementById("heightIn") as HTMLInputElement;
+const weightInput = document.getElementById("weight") as HTMLInputElement;
+const unitsFt = document.getElementById("unitsFt") as HTMLLabelElement;
+const unitsIn = document.getElementById("unitsIn") as HTMLLabelElement;
+const unitsLb = document.getElementById("unitsLb") as HTMLLabelElement;
+
+// add event listener to units 
 unitsSelect.addEventListener("change", () => {
-    // get selected option
     const selectedOption = unitsSelect.options[unitsSelect.selectedIndex] as HTMLOptionElement;
-    // check if selected option is in bodyPartsWithLaterality list
+    
+    // check if selected option is `metric` or `US`
     if (selectedOption.value === "metric") {
+        // check if height input value; convert to metric
+        if (heightInputFt.value) {
+            const heightInInches = getInchesFromFeetAndInches(parseFloat(heightInputFt.value), parseFloat(heightInputIn.value));
+            
+            // convert to display in m, cm
+            let heightInMeters = (heightInInches * 0.0254);
+            let remainder = heightInMeters % 1;
+            let heightInCm = remainder * 100;
+            heightInMeters = Math.floor(heightInMeters);
+            heightInputFt.value = heightInMeters.toString();
+            heightInputIn.value = heightInCm.toString();
+           
+        }
+        // check if weight input value; convert to metric
+        if (weightInput.value) {
+            weightInput.value = (parseFloat(weightInput.value) * 0.45359237).toFixed(2);
+        }
+       
         unitsFt.innerText = "m";
         unitsIn.innerText = "cm";
         unitsLb.innerText = "kg";
     } else {
+        // check if height input value; convert to US
+        if (heightInputFt.value) {
+            const heightInCm = parseFloat(heightInputFt.value)*100 + parseFloat(heightInputIn.value);
+            // convert to display in ft, in
+            let heightInInches = (heightInCm * 0.393701);
+            const feet = Math.floor(heightInInches / 12);
+            const inches = heightInInches % 12;
+            heightInputFt.value = feet.toString();
+            heightInputIn.value = inches.toString();
+        }   
+        if (weightInput.value) {
+            weightInput.value = (parseFloat(weightInput.value) * 2.2046226218).toFixed(2);
+        }
+        
         unitsFt.innerText = "ft";
         unitsIn.innerText = "in";
         unitsLb.innerText = "lb";
     }
 });
+
+function getInchesFromFeetAndInches(feet: number, inches: number): number {
+    return (feet * 12) + inches;
+}
