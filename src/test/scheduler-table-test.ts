b6dc85cf468dfe0e63636618999130cf3c4b0989
vie_bcit <vienna_ly@bcit.ca>
2025-05-26 17:00:47 -0700

    edit sceduler-table-test mock data

--- src/test/scheduler-table-test.ts
+++ src/test/scheduler-table-test.ts
@@ -6,19 +6,19 @@ const mockRows: schedulerTableRowType[] = [
         id: "1",
         patient: "Doe, John",
         procedure: "Arm (Left)",
-        date: "10/3/2025 10:00 AM"
+        date: "10/3/2025 10:00"
     },
     {
         id: "2",
         patient: "Smith, Jane",
         procedure: "Ankle (Right)",
-        date: "10/3/2025 11:00 AM"
+        date: "10/3/2025 11:00"
     },
     {
         id: "3",
         patient: "Johnson, Alice",
         procedure: "Abdomen",
-        date: "10/3/2025 12:00 PM"
+        date: "10/3/2025 12:00"
     }
 ];
 
